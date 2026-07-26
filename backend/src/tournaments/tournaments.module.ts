692a53b92c57fc76c7245e021e70eab3454ee3a5
Cashipro <cashipro.com@gmail.com>
2026-07-27 00:47:12 +0400

    Create tournaments.module.ts

---
+++ backend/src/tournaments/tournaments.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TournamentsController } from './tournaments.controller';
+import { TournamentsService } from './tournaments.service';
+import { Tournament } from './tournament.entity';
+import { TournamentRegistration } from './tournament-registration.entity';
+import { TeamsModule } from '../teams/teams.module';
+import { WalletModule } from '../wallet/wallet.module';
+import { PlayersModule } from '../players/players.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Tournament, TournamentRegistration]),
+    TeamsModule,
+    WalletModule,
+    PlayersModule,
+  ],
+  controllers: [TournamentsController],
+  providers: [TournamentsService],
+  exports: [TournamentsService],
+})
+export class TournamentsModule {}
