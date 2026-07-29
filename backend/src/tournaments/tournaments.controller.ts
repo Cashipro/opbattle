3991384362848c0341851c03c96262014098ca3f
Cashipro <cashipro.com@gmail.com>
2026-07-30 03:36:13 +0400

    Update tournaments.controller.ts

--- backend/src/tournaments/tournaments.controller.ts
+++ backend/src/tournaments/tournaments.controller.ts
@@ -4,16 +4,29 @@ Controller,
 
 Get,
 
-Param
+Param,
+
+Post,
+
+Body
 
 } from '@nestjs/common';
 
 
 
 import {
+
 TournamentsService
+
 } from './tournaments.service';
 
+
+
+import {
+
+JoinService
+
+} from './join.service';
 
 
 
@@ -27,9 +40,13 @@ export class TournamentsController {
 
 constructor(
 
-private tournamentsService:TournamentsService
+private tournamentsService:TournamentsService,
+
+private joinService:JoinService
 
 ){}
+
+
 
 
 
@@ -39,12 +56,9 @@ private tournamentsService:TournamentsService
 
 findAll(){
 
-
 return this.tournamentsService.findAll();
 
-
 }
-
 
 
 
@@ -58,11 +72,40 @@ findOne(
 
 ){
 
-
 return this.tournamentsService.findOne(id);
+
+}
+
+
+
+
+
+
+
+@Post(':id/join')
+
+join(
+
+@Param('id') id:string,
+
+@Body() body:any
+
+){
+
+
+
+return this.joinService.joinTournament(
+
+body.userId,
+
+id
+
+);
 
 
 }
+
+
 
 
 
