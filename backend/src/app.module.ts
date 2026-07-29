4e1a3ae83dd8a2c2b3c7375b3fdb249172cbecdf
Cashipro <cashipro.com@gmail.com>
2026-07-30 03:27:34 +0400

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,11 +4,14 @@ import { ConfigModule } from '@nestjs/config';
 
 import { PrismaModule } from './prisma/prisma.module';
 
+import { AuthModule } from './auth/auth.module';
+
 
 
 @Module({
 
 imports:[
+
 
 ConfigModule.forRoot({
 
@@ -17,11 +20,13 @@ isGlobal:true
 }),
 
 
-PrismaModule
+PrismaModule,
 
 
-],
+AuthModule
 
+
+]
 
 })
 
