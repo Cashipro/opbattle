725037a569f8de3cf40034392543b3dc2bea1f55
Cashipro <cashipro.com@gmail.com>
2026-07-30 03:25:33 +0400

    Create register.dto.ts

---
+++ backend/src/auth/dto/register.dto.ts
@@ -0,0 +1,36 @@
+import {
+IsEmail,
+IsNotEmpty,
+MinLength
+} from 'class-validator';
+
+
+
+export class RegisterDto {
+
+
+@IsNotEmpty()
+
+name:string;
+
+
+
+@IsEmail()
+
+email:string;
+
+
+
+@IsNotEmpty()
+
+pubg_uid:string;
+
+
+
+@MinLength(6)
+
+password:string;
+
+
+
+}
