64a9bb462822aa7c4e81fd90a6a13fa97d5e0c04
Cashipro <cashipro.com@gmail.com>
2026-07-30 22:23:51 +0400

    Create deposit.module.ts

---
+++ backend/src/deposit/deposit.module.ts
@@ -0,0 +1,36 @@
+import { Module } from '@nestjs/common';
+
+import { DepositController } from './deposit.controller';
+
+import { DepositService } from './deposit.service';
+
+import { PrismaModule } from '../prisma/prisma.module';
+
+
+
+@Module({
+
+imports:[
+
+PrismaModule
+
+],
+
+
+controllers:[
+
+DepositController
+
+],
+
+
+providers:[
+
+DepositService
+
+]
+
+})
+
+
+export class DepositModule {}
