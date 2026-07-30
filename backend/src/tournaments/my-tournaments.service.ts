6d00715d45e42ea4852b19fe26ff7d24064027fc
Cashipro <cashipro.com@gmail.com>
2026-07-30 04:27:30 +0400

    Update my-tournaments.service.ts

--- backend/src/tournaments/my-tournaments.service.ts
+++ backend/src/tournaments/my-tournaments.service.ts
@@ -3,9 +3,11 @@ Injectable
 } from '@nestjs/common';
 
 
+
 import {
 PrismaService
 } from '../prisma/prisma.service';
+
 
 
 
@@ -29,6 +31,7 @@ private prisma:PrismaService
 
 
 
+
 async getMyTournaments(
 
 userId:string
@@ -37,7 +40,11 @@ userId:string
 
 
 
-return this.prisma.tournamentSlot.findMany({
+
+
+const slots =
+
+await this.prisma.tournamentSlot.findMany({
 
 where:{
 
@@ -57,7 +64,67 @@ tournament:true,
 
 
 
-team:true
+team:{
+
+
+
+include:{
+
+
+
+slots:{
+
+
+
+orderBy:{
+
+
+slot_number:"asc"
+
+
+},
+
+
+
+include:{
+
+
+user:{
+
+
+select:{
+
+
+id:true,
+
+
+name:true,
+
+
+pubg_uid:true
+
+
+}
+
+
+
+}
+
+
+
+}
+
+
+
+}
+
+
+
+}
+
+
+
+}
 
 
 
@@ -65,10 +132,11 @@ team:true
 
 
 
+
 orderBy:{
 
 
-created_at:'desc'
+created_at:"desc"
 
 
 }
@@ -79,7 +147,87 @@ created_at:'desc'
 
 
 
+
+
+
+
+
+return slots.map(item=>({
+
+
+
+tournament:{
+
+
+id:item.tournament.id,
+
+
+name:item.tournament.name,
+
+
+entry_fee:item.tournament.entry_fee,
+
+
+reward:item.tournament.reward,
+
+
+start_date:item.tournament.start_date,
+
+
+start_time:item.tournament.start_time,
+
+
+status:item.tournament.status
+
+
+},
+
+
+
+
+
+team:{
+
+
+id:item.team.id,
+
+
+name:item.team.name,
+
+
+team_number:item.team.team_number,
+
+
+
+players:item.team.slots.map(slot=>({
+
+
+slot_number:slot.slot_number,
+
+
+user:slot.user
+
+
+}))
+
+
+
 }
+
+
+
+}));
+
+
+
+
+
+
+}
+
+
+
+
 
 
 
