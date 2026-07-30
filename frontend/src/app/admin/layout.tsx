e86e47d6dda4370a7dcb79119e06f40b50cc5455
Cashipro <cashipro.com@gmail.com>
2026-07-30 18:44:50 +0400

    Update layout.tsx

--- frontend/src/app/admin/layout.tsx
+++ frontend/src/app/admin/layout.tsx
@@ -1,27 +1,50 @@
-"use client";
+import AdminSidebar from "./components/AdminSidebar";
 
-import AdminSidebar from "@/components/AdminSidebar";
 
 export default function AdminLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <div className="flex min-h-screen bg-black text-white">
-      <AdminSidebar />
 
-      <main
-        className="
-        flex-1
-        bg-black
-        p-6
-        md:p-10
-        overflow-y-auto
-        "
-      >
-        {children}
-      </main>
-    </div>
-  );
+children,
+
+}: {
+
+children: React.ReactNode;
+
+}) {
+
+
+return (
+
+<div className="
+min-h-screen
+bg-black
+text-white
+flex
+">
+
+
+<AdminSidebar />
+
+
+
+<main className="
+flex-1
+ml-72
+p-6
+md:p-10
+">
+
+
+{children}
+
+
+
+</main>
+
+
+
+</div>
+
+);
+
+
 }
