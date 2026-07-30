f4177131bf36c1b6db9e1c51be66d46cecc82bcd
Cashipro <cashipro.com@gmail.com>
2026-07-31 03:46:48 +0400

    Update admin.module.ts

--- backend/src/admin/admin.module.ts
+++ backend/src/admin/admin.module.ts
@@ -4,28 +4,100 @@ import { PrismaService } from '../prisma/prisma.service';
 
 import { AdminTournamentService } from './admin-tournament.service';
 
+import { AdminTournamentController } from './admin-tournament.controller';
+
 import { TeamRoomService } from '../tournaments/team-room.service';
+
+import { AutoPlannerService } from '../tournaments/auto-planner.service';
+
+import { NextRoundService } from '../tournaments/next-round.service';
 
 
 @Module({
 
-  providers:[
-
-    PrismaService,
-
-    AdminTournamentService,
-
-    TeamRoomService
-
-  ],
+imports:[],
 
 
-  exports:[
+controllers:[
 
-    AdminTournamentService
+AdminTournamentController
 
-  ]
+],
+
+
+providers:[
+
+PrismaService,
+
+AdminTournamentService,
+
+TeamRoomService,
+
+AutoPlannerService,
+
+NextRoundService
+
+],
+
+
+exports:[
+
+AdminTournamentService
+
+]
 
 })
+
+
+export class AdminModule {}import { Module } from '@nestjs/common';
+
+import { PrismaService } from '../prisma/prisma.service';
+
+import { AdminTournamentService } from './admin-tournament.service';
+
+import { AdminTournamentController } from './admin-tournament.controller';
+
+import { TeamRoomService } from '../tournaments/team-room.service';
+
+import { AutoPlannerService } from '../tournaments/auto-planner.service';
+
+import { NextRoundService } from '../tournaments/next-round.service';
+
+
+@Module({
+
+imports:[],
+
+
+controllers:[
+
+AdminTournamentController
+
+],
+
+
+providers:[
+
+PrismaService,
+
+AdminTournamentService,
+
+TeamRoomService,
+
+AutoPlannerService,
+
+NextRoundService
+
+],
+
+
+exports:[
+
+AdminTournamentService
+
+]
+
+})
+
 
 export class AdminModule {}
