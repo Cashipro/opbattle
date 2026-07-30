cf3b5c9641269a308eb7bff93e8ff15a7f9e9612
Cashipro <cashipro.com@gmail.com>
2026-07-30 18:59:39 +0400

    Update layout.tsx

--- frontend/src/app/admin/layout.tsx
+++ frontend/src/app/admin/layout.tsx
@@ -1,15 +1,145 @@
+"use client";
+
+
+import {
+  useEffect,
+  useState
+} from "react";
+
+
+import {
+  useRouter
+} from "next/navigation";
+
+
 import AdminSidebar from "./components/AdminSidebar";
+
+
+
+
 
 
 export default function AdminLayout({
 
-children,
+children
 
-}: {
+}:{
 
-children: React.ReactNode;
+children:React.ReactNode
 
-}) {
+}){
+
+
+
+const router = useRouter();
+
+
+const [checking,setChecking] =
+useState(true);
+
+
+
+
+
+
+
+
+useEffect(()=>{
+
+
+
+const token =
+localStorage.getItem("token");
+
+
+
+const user =
+JSON.parse(
+
+localStorage.getItem("user") || "{}"
+
+);
+
+
+
+
+
+
+if(!token){
+
+
+router.push("/login");
+
+
+return;
+
+
+}
+
+
+
+
+
+
+if(
+
+user.role !== "admin"
+
+){
+
+
+router.push("/dashboard");
+
+
+return;
+
+
+}
+
+
+
+
+
+setChecking(false);
+
+
+
+},[]);
+
+
+
+
+
+
+
+
+if(checking){
+
+
+return (
+
+<div className="
+min-h-screen
+bg-black
+text-white
+flex
+items-center
+justify-center
+">
+
+Checking admin access...
+
+</div>
+
+);
+
+
+}
+
+
+
+
+
 
 
 return (
@@ -26,6 +156,8 @@ flex
 
 
 
+
+
 <main className="
 flex-1
 ml-72
@@ -37,8 +169,9 @@ md:p-10
 {children}
 
 
-
 </main>
+
+
 
 
 
