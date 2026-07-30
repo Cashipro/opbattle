7a0161cfa18ed9c076130b5ed56f142883efde3e
Cashipro <cashipro.com@gmail.com>
2026-07-30 08:10:55 +0400

    Update api.ts

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,110 +1,45 @@
 import axios from "axios";
 
-
 const api = axios.create({
-
   baseURL:
     process.env.NEXT_PUBLIC_API_URL ||
-    "http://localhost:4000/api",
+    "https://opbattle-production.up.railway.app",
 
   headers: {
-
     "Content-Type": "application/json",
-
   },
-
 });
 
-
-
-
 // REQUEST INTERCEPTOR
-
 api.interceptors.request.use(
+  (config) => {
+    const token =
+      typeof window !== "undefined"
+        ? localStorage.getItem("token")
+        : null;
 
-(config)=>{
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
 
-
-const token =
-typeof window !== "undefined"
-?
-localStorage.getItem("token")
-:
-null;
-
-
-
-if(token){
-
-config.headers.Authorization =
-`Bearer ${token}`;
-
-}
-
-
-
-return config;
-
-
-},
-
-
-(error)=>{
-
-return Promise.reject(error);
-
-}
-
-
+    return config;
+  },
+  (error) => Promise.reject(error)
 );
-
-
-
-
-
-
 
 // RESPONSE INTERCEPTOR
-
 api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      if (typeof window !== "undefined") {
+        localStorage.removeItem("token");
+        localStorage.removeItem("user");
+      }
+    }
 
-(response)=>{
-
-return response;
-
-},
-
-
-(error)=>{
-
-
-if(error.response?.status===401){
-
-
-if(typeof window !== "undefined"){
-
-localStorage.removeItem("token");
-
-localStorage.removeItem("user");
-
-}
-
-
-}
-
-
-
-return Promise.reject(error);
-
-
-}
-
-
-
+    return Promise.reject(error);
+  }
 );
-
-
-
-
 
 export default api;
